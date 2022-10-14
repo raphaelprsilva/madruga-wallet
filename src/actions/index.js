66295f2345d1774c324714304372ac1a7f1073f7
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-10-14 08:40:46 -0300

    feat: add redux config

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
-// Coloque aqui suas actions
+export const DO_LOGIN = 'DO_LOGIN';
+
+export const login = (email) => ({
+  type: DO_LOGIN,
+  email,
+});
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,21 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { DO_LOGIN } from '../actions/index';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case DO_LOGIN: {
+    const { email } = action;
+    return {
+      ...state,
+      email,
+    };
+  }
+  default:
+    return state;
+  }
+};
+
+export default user;
