5fa2b6982ceea97484f044f2f226edc2e8d6079f
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-10-14 08:53:48 -0300

    feat: add Wallet page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Login } from './pages';
+import { Login, Wallet } from './pages';
 
 const App = () => (
   <Switch>
     <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+    <Route path="/carteira" render={ (props) => <Wallet { ...props } /> } />
   </Switch>
 );
 
