9cfbd0bc007baa6532564c3ae781d127074cc470
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-10-19 09:19:54 -0300

    feat: add company Logo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+
 import LoginForm from '../components/LoginForm';
+import MadrugaWalletLogo from '../assets/images/madruga-wallet-black-logo.svg';
 
 class Login extends Component {
   render() {
     return (
-      <LoginForm { ...this.props } />
+      <div>
+        <img
+          src={ MadrugaWalletLogo }
+          alt="Imagem da logo da Madruga Wallet"
+          style={ { width: '200px', marginBottom: '20px' } }
+        />
+        <LoginForm { ...this.props } />
+      </div>
     );
   }
 }
