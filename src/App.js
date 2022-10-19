05bec06f39c50490a291eb8d7c9e8e196f857563
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-10-19 09:20:59 -0300

    refactor: change to class Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+
 import { Login, Wallet } from './pages';
 
-const App = () => (
-  <Switch>
-    <Route exact path="/" render={ (props) => <Login { ...props } /> } />
-    <Route path="/carteira" render={ (props) => <Wallet { ...props } /> } />
-  </Switch>
-);
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+        <Route path="/carteira" render={ (props) => <Wallet { ...props } /> } />
+      </Switch>
+    );
+  }
+}
 
 export default App;
